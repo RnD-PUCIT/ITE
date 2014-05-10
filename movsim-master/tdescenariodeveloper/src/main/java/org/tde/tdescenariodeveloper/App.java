fa854583611d77d3123dbd1d0b2e9bb7e7a7b1cf
Shmeel <shmeelahmad@gmail.com>
2014-05-10 10:47:37 +0500

    Revert "location changed"
    This reverts commit f3323b046ce056cc4bf6a6c1bcb315bc7197cfc8.

--- movsim-master/tdescenariodeveloper/src/main/java/org/tde/tdescenariodeveloper/App.java
+++ movsim-master/tdescenariodeveloper/src/main/java/org/tde/tdescenariodeveloper/App.java
@@ -9,7 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-    	System.out.println("Shmeel");
     	GraphicsHelper.setWindowsUI();
     	AppFrame fr=new AppFrame();
     	fr.setVisible(true);
